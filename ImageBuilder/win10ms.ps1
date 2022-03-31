df927c439844a8c8ff242db0c52defaa7a6e5580
Bruna Moreira Bruno <79637926+brmoreir@users.noreply.github.com>
2022-03-31 16:50:38 +0100

    Update win10ms.ps1
    This script is not working. 
    I'm getting 'New-AzRoleDefinition: Operation returned an invalid status code 'BadRequest'' error when running line 83.

---
+++ ImageBuilder/win10ms.ps1
@@ -0,0 +1,90 @@
+##WARNING: Still needs work. Did not function properly. 
+
+#Reference documentation --> https://github.com/azure/azvmimagebuilder/tree/main/solutions/14_Building_Images_WVD
+
+#PreReqs
+# Register for Azure Image Builder Feature
+Register-AzProviderFeature -FeatureName VirtualMachineTemplatePreview -ProviderNamespace Microsoft.VirtualMachineImages
+
+Get-AzProviderFeature -FeatureName VirtualMachineTemplatePreview -ProviderNamespace Microsoft.VirtualMachineImages
+# wait until RegistrationState is set to 'Registered'
+
+# check you are registered for the providers, ensure RegistrationState is set to 'Registered'.
+Get-AzResourceProvider -ProviderNamespace Microsoft.VirtualMachineImages
+Get-AzResourceProvider -ProviderNamespace Microsoft.Storage 
+Get-AzResourceProvider -ProviderNamespace Microsoft.Compute
+Get-AzResourceProvider -ProviderNamespace Microsoft.KeyVault
+
+# If they do not saw registered, run the commented out code below.
+
+## Register-AzResourceProvider -ProviderNamespace Microsoft.VirtualMachineImages
+## Register-AzResourceProvider -ProviderNamespace Microsoft.Storage
+## Register-AzResourceProvider -ProviderNamespace Microsoft.Compute
+## Register-AzResourceProvider -ProviderNamespace Microsoft.KeyVault
+
+
+
+#Set up environment and variables
+# Step 1: Import module
+Import-Module Az.Accounts
+
+# Step 2: get existing context
+$currentAzContext = Get-AzContext
+
+# destination image resource group
+$imageResourceGroup="wvdImageDemoRg"
+
+# location (see possible locations in main docs)
+$location="westus2"
+
+# your subscription, this will get your current subscription
+$subscriptionID=$currentAzContext.Subscription.Id
+
+# image template name
+$imageTemplateName="wvd10ImageTemplate01"
+
+# distribution properties object name (runOutput), i.e. this gives you the properties of the managed image on completion
+$runOutputName="sigOutput"
+
+# create resource group
+New-AzResourceGroup -Name $imageResourceGroup -Location $location
+
+
+
+#Permissions, create user identity and role for AIB
+#Step 1: Create user identity
+# setup role def names, these need to be unique
+$timeInt=$(get-date -UFormat "%s")
+$imageRoleDefName="Azure Image Builder Image Def"+$timeInt
+$idenityName="aibIdentity"+$timeInt
+
+## Add AZ PS modules to support AzUserAssignedIdentity and Az AIB
+'Az.ImageBuilder', 'Az.ManagedServiceIdentity' | ForEach-Object {Install-Module -Name $_ -AllowPrerelease}
+
+# create identity
+New-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $idenityName
+
+$idenityNameResourceId=$(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $idenityName).Id
+$idenityNamePrincipalId=$(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $idenityName).PrincipalId
+
+#Step 2: Assign permissions for identity to distribute images
+
+$aibRoleImageCreationUrl="https://raw.githubusercontent.com/danielsollondon/azvmimagebuilder/master/solutions/12_Creating_AIB_Security_Roles/aibRoleImageCreation.json"
+$aibRoleImageCreationPath = "aibRoleImageCreation.json"
+
+# download config
+Invoke-WebRequest -Uri $aibRoleImageCreationUrl -OutFile $aibRoleImageCreationPath -UseBasicParsing
+
+((Get-Content -path $aibRoleImageCreationPath -Raw) -replace '<subscriptionID>',$subscriptionID) | Set-Content -Path $aibRoleImageCreationPath
+((Get-Content -path $aibRoleImageCreationPath -Raw) -replace '<rgName>', $imageResourceGroup) | Set-Content -Path $aibRoleImageCreationPath
+((Get-Content -path $aibRoleImageCreationPath -Raw) -replace 'Azure Image Builder Service Image Creation Role', $imageRoleDefName) | Set-Content -Path $aibRoleImageCreationPath
+
+# create role definition
+New-AzRoleDefinition -InputFile  ./aibRoleImageCreation.json
+
+# grant role definition to image builder service principal
+New-AzRoleAssignment -ObjectId $idenityNamePrincipalId -RoleDefinitionName $imageRoleDefName -Scope "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup"
+
+### NOTE: If you see this error: 'New-AzRoleDefinition: Role definition limit exceeded. No more role definitions can be created.' See this article to resolve:
+https://docs.microsoft.com/en-us/azure/role-based-access-control/troubleshooting
+
